67b518b3a97bddfe773b64eb2a85928d5fb3e80a
ASDSausage <asdsausage@gmail.com>
2015-05-14 19:21:51 +0300

    Kissat on koiria

--- src/dishcloth/game/Program.java
+++ src/dishcloth/game/Program.java
@@ -4,8 +4,12 @@ public class Program {
 
 	public static void main(String[] args) {
 
+
+
 		// Just don't care about this, okay? :D
 		DummyMain.run();
 		System.out.println("trrföhgfd");
+
+		System.out.println("Kissat on koiria");
 	}
 }
