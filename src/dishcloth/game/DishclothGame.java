905a008377faa61cffd7f1e905cec05aab052c8e
ASDSausage <asdsausage@gmail.com>
2015-05-21 16:06:54 +0300

    Implemented basic game bare-bones to test window initialization

---
+++ src/dishcloth/game/DishclothGame.java
@@ -0,0 +1,53 @@
+package dishcloth.game;
+
+import dishcloth.engine.AGame;
+import dishcloth.engine.rendering.Renderer;
+
+/**
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * DishclothGame.java
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * <p>
+ * TODO: Description
+ * <p>
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Created by ASDSausage on 21.5.2015
+ */
+
+public class DishclothGame extends AGame {
+
+	@Override
+	public void initialize() {
+
+	}
+
+	@Override
+	public void loadContent() {
+
+	}
+
+	@Override
+	public void update(float delta) {
+
+	}
+
+	@Override
+	public void fixedUpdate() {
+
+	}
+
+	@Override
+	public void render(Renderer renderer) {
+
+	}
+
+	@Override
+	public void shutdown() {
+
+	}
+
+	@Override
+	public void unloadContent() {
+
+	}
+}
