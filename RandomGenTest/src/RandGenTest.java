c583b7ec9e305672818db5053f1215e46839cc8e
Lassi <lassi.kuisma1@gmail.com>
2015-06-21 15:10:59 +0300

    :)

--- RandomGenTest/src/RandGenTest.java
+++ RandomGenTest/src/RandGenTest.java
@@ -1,5 +1,18 @@
+import java.util.Random;
+
 /**
  * Created by Lassi on 21.6.2015.
  */
 public class RandGenTest {
+
+	public static void main(String[] args){
+		System.out.println("Testing...");
+
+
+
+		long seed =1L;
+		Random random = new Random( seed );
+
+	}
+
 }
